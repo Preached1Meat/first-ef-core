a44f82ad0aef939b35fb93cb8e539a7699c48cc3
stijn <stijn.nel@gmail.com>
2021-08-10 12:47:37 +0200

    Initial Commit

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+	public class SamuraiContext : DbContext
+	{
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// Don't hardcode into context
+			// For Demo purposes
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SamuraiAppData");
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
