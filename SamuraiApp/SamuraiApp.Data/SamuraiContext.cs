c6785135bdcf19c12f20046a26a487f1e895feed
stijn <preachedmeat@gmail.com>
2021-08-10 15:50:51 +0200

    Add Battle domain class
    Add new battle domain class as well as a many to many relationship to the samurai class. To expose added a dbset of the battle to the context

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -13,6 +13,8 @@ namespace SamuraiApp.Data
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 
+		public DbSet<Battle> Battles { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			// Don't hardcode into context
